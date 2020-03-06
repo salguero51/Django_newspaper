3182215f1736909d9ff593ba0c9073d010b20268
salguero51 <salguero51@hotmail.es>
2020-03-06 09:54:26 +0100

    Permisos y Autorización

--- articles/models.py
+++ articles/models.py
@@ -12,8 +12,8 @@ class Article(models.Model):
         on_delete=models.CASCADE,
     )
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-def get_absolute_url(self):
-    return reverse('article_detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
