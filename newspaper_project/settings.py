db12626ee904659b568ec25f35acd6d9f09d0225
salguero51 <salguero51@hotmail.es>
2020-03-06 09:25:49 +0100

    email

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -133,5 +133,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
